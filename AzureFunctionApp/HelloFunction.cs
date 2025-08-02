52f67e1aa07ff7c81732ebf437f2fa71468e29ae
Raj Kumar Arora <Raj Kumar Arora@RAJKUMARARORA-D>
2025-08-03 01:26:34 +0200

    Changing to Async version

--- AzureFunctionApp/HelloFunction.cs
+++ AzureFunctionApp/HelloFunction.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace AzureFunctionApp
 {
@@ -15,20 +16,18 @@ namespace AzureFunctionApp
         }
 
         [Function("HelloFunction")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            //response.WriteString("Welcome to Azure Functions!");
-
             string name = req.Query["name"];
             if (string.IsNullOrWhiteSpace(name))
-                response.WriteString("Hello, world!");
+                await response.WriteStringAsync("Hello, world!");
             else 
-                response.WriteString($"Hello, {name}!");
+                await response.WriteStringAsync($"Hello, {name}!");
 
             return response;
         }
